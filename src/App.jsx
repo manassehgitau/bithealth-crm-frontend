c60b1422a664f7e4d5a1f68da991a6b9ef31ad5d
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-03-21 23:35:11 +0300

    feat: Enable vercel analytics in root dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import AdminPage from './pages/AdminPage'
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
-    <Analytics />
       <AdminPage />
       
       
